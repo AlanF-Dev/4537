4db888b11d4c18f8b1fb955ae775b31e3ceaaaf5
Alan <alan.fung4@gmail.com>
2023-10-31 10:03:06 -0700

    removed unused file

--- client.js
+++
@@ -1,8 +0,0 @@
-function analyze() {
-    fetch(insertEndPoint, {
-        method: "POST",
-        mode: "cors"
-    })
-        .then(res => res.json())
-        .then(json => console.log(json))   
-}
