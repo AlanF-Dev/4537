4610377446935296a487f1c54484dd6804d66774
Usman Arshad <uarshad2012@gmail.com>
2023-10-31 09:11:48 -0700

    backend

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
 const http = require('http');
+const querystring = require('querystring');
 const url = require('url');
-const fs = require('fs');
-const port = process.env.PORT || 3000;
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type' : 'text/html'});
+class MyClassificationPipeline {
+    static task = 'text-classification';
+    static model = 'Xenova/distilbert-base-uncased-finetuned-sst-2-english';
+    static instance = null;
 
-    let q = url.parse(req.url, true);
-    if(q.pathname == "/") {
-        fs.readFile('index.html', function (err, html) {
-            if (err) throw err;    
-            res.write(html);
-            res.end()
-        });
+    static async getInstance(progress_callback = null) {
+        if (this.instance === null) {
+        // Dynamically import the Transformers.js library
+        let { pipeline, env } = await import('@xenova/transformers');
+
+        // NOTE: Uncomment this to change the cache directory
+        // env.cacheDir = './.cache';
+
+        this.instance = pipeline(this.task, this.model, { progress_callback });
+        }
+
+        return this.instance;
     }
-}).listen(port);
+}
 
-console.log("Application listening on port " + port);
+
+// Define the HTTP server
+const server = http.createServer();
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Listen for requests made to the server
+server.on('request', async (req, res) => {
+  // Parse the request URL
+  const parsedUrl = url.parse(req.url);
+
+  // Extract the query parameters
+  const { text } = querystring.parse(parsedUrl.query);
+
+  // Set the response headers
+  res.setHeader('Content-Type', 'application/json');
+
+  let response;
+  if (parsedUrl.pathname === '/classify' && text) {
+    const classifier = await MyClassificationPipeline.getInstance();
+    response = await classifier(text);
+    res.statusCode = 200;
+  } else {
+    response = { 'error': 'Bad request' }
+    res.statusCode = 400;
+  }
+
+  // Send the JSON response
+  res.end(JSON.stringify(response));
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
